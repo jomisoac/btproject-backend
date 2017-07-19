9dadc64b194021bf437d2cc65ccc25238d76c241
Jose Miguel Soto A <jomisoac@gmail.com>
2017-07-19 00:53:21 -0500

    Update migration user admin

--- db/migrations/20171807234120_user_admin.js
+++ db/migrations/20171807234120_user_admin.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema
-    .raw("INSERT INTO user (username, password, email, rol) VALUES ('vsadmin', encrypt('1234'), 'viajaseguroapp@hotmail.com', 'SUPER_ADM')  ;")
+    .raw("INSERT INTO user (username, password, email, rol) VALUES ('admin', encrypt('1234'), 'jomisoac@gmail.com', 'SUPER_ADM')  ;")
   ])
 };
 
