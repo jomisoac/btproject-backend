07769f7189e0397349ff55d929d4a318aaf7995c
Jose M Soto <jomisoac@gmail.com>
2017-09-14 00:16:42 -0500

    make it better

--- config/sockets.js
+++ config/sockets.js
@@ -1,13 +1,13 @@
 /**
  * WebSocket Server Settings
  */
-
+var redis_url = require('url').parse(process.env.REDISTOGO_URL);
 module.exports.sockets = {
     // adapter: 'memory',
   adapter: 'socket.io-redis',
-  host: 'cowfish.redistogo.com',
-  port: 9011,
-  pass: '',
+  host: redis_url.hostname,
+  port: redis_url.port,
+  pass: redis_url.auth.split(":")[1],
   db: 'sails',
     origins: '*:*',
     onlyAllowOrigins: '*:*',
