b29800cff81c01e1fb8a0f024e49e6ca04280ddd
Jose M Soto <jomisoac@gmail.com>
2017-09-14 00:23:01 -0500

    make it better

--- config/connections.js
+++ config/connections.js
@@ -4,31 +4,33 @@
 
 module.exports.connections = {
 
-  /***************************************************************************
-  * Local disk storage for DEVELOPMENT ONLY                                  *
-  ***************************************************************************/
-  localDiskDb: {
-    adapter: 'sails-disk'
-  },
+    /***************************************************************************
+     * Local disk storage for DEVELOPMENT ONLY                                  *
+     ***************************************************************************/
+    localDiskDb: {
+        adapter: 'sails-disk'
+    },
 
-  /***************************************************************************
-  * MySQL                                                                    *
-  ***************************************************************************/
-  mysqlServer: {
-    adapter: 'sails-mysql',
-    host: process.env.CLEARDB_DATABASE_URL,
-    database: process.env.CLEARDB_NAME//optional
-  },
+    /***************************************************************************
+     * MySQL                                                                    *
+     ***************************************************************************/
+    mysqlServer: {
+        adapter: 'sails-mysql',
+        host: process.env.CLEARDB_DATABASE_URL,
+        user: process.env.CLEARDB_USER,
+        password: process.env.JAWSDB_PASS,
+        database: process.env.CLEARDB_NAME//optional
+    },
 
 
-  /***************************************************************************
-  * Redis                                                                    *
-  ***************************************************************************/
-  redis: {
-    adapter: 'sails-redis',
-    port: 9011,
-    host: 'cowfish.redistogo.com',
-    password: 'bccd85e4c5326f87a8b17cdb2561bff8'
-  },
+    /***************************************************************************
+     * Redis                                                                    *
+     ***************************************************************************/
+    redis: {
+        adapter: 'sails-redis',
+        port: 9011,
+        host: 'cowfish.redistogo.com',
+        password: 'bccd85e4c5326f87a8b17cdb2561bff8'
+    },
 
 };
