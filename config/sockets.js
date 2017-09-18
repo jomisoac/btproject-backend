3e47239043d0593a7520525bcf9eb4afb00fb06e
Jose M Soto <jomisoac@gmail.com>
2017-09-18 01:43:23 -0500

    update

--- config/sockets.js
+++ config/sockets.js
@@ -1,13 +1,13 @@
 /**
  * WebSocket Server Settings
  */
-var redis_url = require('url').parse(process.env.REDISTOGO_URL);
+
 module.exports.sockets = {
     // adapter: 'memory',
   adapter: 'socket.io-redis',
-  host: redis_url.hostname,
-  port: redis_url.port,
-  pass: redis_url.auth.split(":")[1],
+  host: '127.0.0.1',
+  port: 6379,
+  pass: '',
   db: 'sails',
     origins: '*:*',
     onlyAllowOrigins: '*:*',
